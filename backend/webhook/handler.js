dfb986ff36bdead1eda4f47364c656f4487721f7
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-04-09 18:24:06 +0200

    fix facebook data transform

--- backend/webhook/handler.js
+++ backend/webhook/handler.js
@@ -159,36 +159,38 @@ class WebhookHandler {
    * @protected
    */
   _transformFacebookData (message) {
+    let activities = []
     if (message.object === 'page') {
-      let activities = []
-      message.entry.changes.forEach(async (change) => {
-        if (change.field === 'feed') {
-          switch (change.value.item) {
-            case 'share':
-              if (change.value.verb === 'add') {
-                activities.push({
-                  type: 'Message',
-                  content: {
-                    message: change.value.message,
-                    link: change.value.link
-                  },
-                  external: {
-                    type: 'facebook',
-                    id: change.value.post_id,
-                    original: change
-                  }
-                })
-              }
-              break
+      message.entry.forEach(entryItem => {
+        entryItem.forEach(async (change) => {
+          if (change.field === 'feed') {
+            switch (change.value.item) {
+              case 'share':
+                if (change.value.verb === 'add') {
+                  activities.push({
+                    type: 'Message',
+                    content: {
+                      message: change.value.message,
+                      link: change.value.link
+                    },
+                    external: {
+                      type: 'facebook',
+                      id: change.value.post_id,
+                      original: change
+                    }
+                  })
+                }
+                break
+            }
           }
-        }
+        })
       })
-      if (activities.length === 0) {
-        // save unhandled data to file for later usage/analysis
-        fs.appendFile('facebook-data.txt', JSON.stringify(message) + '\n\n')
-      }
-      return activities
     }
+    if (activities.length === 0) {
+      // save unhandled data to file for later usage/analysis
+      fs.appendFile('facebook-data.txt', JSON.stringify(message) + '\n\n')
+    }
+    return activities
   }
 }
 
