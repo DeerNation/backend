52201192d1b816871102684820a20c8338db5361
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-04-14 15:49:50 +0200

    use one logger with message prefix

--- backend/logger.js
+++ backend/logger.js
@@ -25,11 +25,11 @@
  */
 
 const { format, transports, loggers } = require('winston')
-const { combine, timestamp, label, printf, colorize, align, splat } = format
+const { combine, timestamp, printf, colorize, align, splat } = format
 const maxModuleLength = 25
 
 const myFormat = printf(info => {
-  return `${info.timestamp.replace(/[T|Z]/g, ' ')} [${info.label}] ${info.level}: ${info.message}`
+  return `${info.timestamp.replace(/[T|Z]/g, ' ')}${info.level} - ${info.message}`
 })
 
 const defaultLoggerConfig = {
@@ -50,53 +50,62 @@ if (process.env.ENV === 'dev') {
   }))
 }
 
+// create the logger
+if (process.env.ENV === 'dev') {
+  // console.log(module, config)
+  defaultLoggerConfig.format = combine(
+    colorize({all: true}),
+    splat(),
+    timestamp(),
+    myFormat
+  )
+} else {
+  defaultLoggerConfig.format = combine(
+    format.padLevels(),
+    splat(),
+    timestamp(),
+    align(),
+    myFormat
+  )
+}
+const logger = loggers.add('nameLogger', defaultLoggerConfig)
+
 module.exports = function (fileName) {
-  if (!loggers.has(fileName)) {
-    // create new logger
-    let config = Object.assign({}, defaultLoggerConfig)
+  // create new logger
 
-    // normalize filename
-    let parts = fileName.replace(/[/]/g, '.').split('.')
-    parts.pop()
-    // remove filetype
-    const start = Math.max(parts.indexOf('backend'), 0)
-    let module = parts.slice(start).join('.')
+  // normalize filename
+  let parts = fileName.replace(/[/]/g, '.').split('.')
+  parts.pop()
+  // remove filetype
+  const start = Math.max(parts.indexOf('backend'), 0)
+  let labelString = parts.slice(start).join('.')
 
-    let labelString = module
-    if (labelString.length > maxModuleLength) {
-      let gain = labelString.length - maxModuleLength
-      let parts = labelString.split('.')
-      for (let i = 0; i < parts.length; i++) {
-        gain -= parts[i].length - 1
-        parts[i] = parts[i].substr(0, 1)
-        if (gain <= 0) {
-          break
-        }
+  if (labelString.length > maxModuleLength) {
+    let gain = labelString.length - maxModuleLength
+    let parts = labelString.split('.')
+    for (let i = 0; i < parts.length; i++) {
+      gain -= parts[i].length - 1
+      parts[i] = parts[i].substr(0, 1)
+      if (gain <= 0) {
+        break
       }
-      labelString = parts.join('.')
     }
-    labelString = `${labelString}`.padStart(maxModuleLength, '.')
-
-    if (process.env.ENV === 'dev') {
-      // console.log(module, config)
-      config.format = combine(
-        colorize({all: true}),
-        label({label: labelString}),
-        splat(),
-        timestamp(),
-        myFormat
-      )
-    } else {
-      config.format = combine(
-        format.padLevels(),
-        label({label: labelString}),
-        splat(),
-        timestamp(),
-        align(),
-        myFormat
-      )
-    }
-    return loggers.add(fileName, config)
+    labelString = parts.join('.')
   }
-  return loggers.get(fileName)
+  labelString = `${labelString}`.padStart(maxModuleLength, '.')
+
+  return {
+    info: function (msg, vars) {
+      logger.info(`[${labelString}]: ${msg}`, vars)
+    },
+    debug: function (msg, vars) {
+      logger.debug(`[${labelString}]: ${msg}`, vars)
+    },
+    error: function (msg, vars) {
+      logger.error(`[${labelString}]: ${msg}`, vars)
+    },
+    warning: function (msg, vars) {
+      logger.warning(`[${labelString}]: ${msg}`, vars)
+    }
+  }
 }
