f7329be122c67d685ece441d71c2f28e0874e129
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-10-16 22:43:51 +0200

    use promises and log errors

--- backend/crawler/iCal.js
+++ backend/crawler/iCal.js
@@ -31,59 +31,69 @@ const logger = require('../logger')(__filename)
 class ICal {
   constructor (file) {
     this.__uri = file
-    this.__parsedData = null
   }
 
   parse () {
     if (this.__uri.startsWith('http')) {
-      this.__parsedData = iCal.fromURL(this.__uri)
+      return new Promise((resolve, reject) => {
+        iCal.fromURL(this.__uri, {}, (err, data) => {
+          if (err) {
+            reject(new Error(err))
+          } else {
+            resolve(data)
+          }
+        })
+      })
     } else {
-      this.__parsedData = iCal.parseFile(this.__uri)
+      return Promise.resolve(iCal.parseFile(this.__uri))
     }
   }
 
   update () {
     logger.info('update iCal events from %s', this.__uri)
-    this.parse()
-    let now = new Date()
+    this.parse().then(data => {
+      let now = new Date()
 
-    for (let k in this.__parsedData) {
-      if (this.__parsedData.hasOwnProperty(k)) {
-        let ev = this.__parsedData[k]
-        if (ev.type === 'VEVENT' && ev.start >= now) {
-          let event = {
-            ref: {
-              id: ev.uid,
-              type: 'ics',
-              original: ev
-            },
-            type: 'event',
-            content: {
-              name: ev.summary,
-              categories: ev.categories,
-              description: ev.description,
-              start: moment(ev.start).format(),
-              end: moment(ev.end).format()
+      for (let k in data) {
+        if (this.__parsedData.hasOwnProperty(k)) {
+          let ev = data[k]
+          if (ev.type === 'VEVENT' && ev.start >= now) {
+            let event = {
+              ref: {
+                id: ev.uid,
+                type: 'ics',
+                original: ev
+              },
+              type: 'event',
+              content: {
+                name: ev.summary,
+                categories: ev.categories,
+                description: ev.description,
+                start: moment(ev.start).format(),
+                end: moment(ev.end).format()
+              }
             }
-          }
-          if (ev.location) {
-            event.content.location = ev.location
-          }
-          if (ev.hasOwnProperty('organizer')) {
-            let orga = ev.organizer.params.CN.trim()
-            if (ev.organizer.val) {
-              orga += ' ' + ev.organizer.val.trim()
+            if (ev.location) {
+              event.content.location = ev.location
             }
-            if (orga.endsWith(':')) {
-              orga = orga.substring(0, orga.length - 1)
+            if (ev.hasOwnProperty('organizer')) {
+              let orga = ev.organizer.params.CN.trim()
+              if (ev.organizer.val) {
+                orga += ' ' + ev.organizer.val.trim()
+              }
+              if (orga.endsWith(':')) {
+                orga = orga.substring(0, orga.length - 1)
+              }
+              orga = orga.substring(1, orga.length - 1)
+              event.content.organizer = orga
             }
-            orga = orga.substring(1, orga.length - 1)
-            event.content.organizer = orga
+            channelHandler.publish({user: '135dd849-9cb6-466a-9a2b-688ae21b6cdf'}, 'hbg.channel.events.public', event)
           }
-          channelHandler.publish({user: '135dd849-9cb6-466a-9a2b-688ae21b6cdf'}, 'hbg.channel.events.public', event)
         }
       }
-    }
+    }).catch(err => {
+      logger.error(err);
+    })
   }
 }
 
